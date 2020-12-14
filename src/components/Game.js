6f1eaef543c015b9ad1776c36a0c206529715635
councit <councit@gmail.com>
2020-12-13 19:28:29 -0800

    added toggle and improved img load speeds

--- src/components/Game.js
+++ src/components/Game.js
@@ -5,6 +5,7 @@ import { motion } from "framer-motion";
 import { useDispatch } from "react-redux";
 import { loadDetail } from "../actions/detailActions";
 import { Link } from "react-router-dom";
+import { smallImage } from "./util";
 
 const Game = ({ name, released, image, id }) => {
   //Load Detail
@@ -18,7 +19,7 @@ const Game = ({ name, released, image, id }) => {
       <Link to={`/game/${id}`}>
         <h3>{name}</h3>
         <p>{released}</p>
-        <img src={image} alt="game backgrounds"></img>
+        <img src={smallImage(image, 640)} alt="game backgrounds"></img>
       </Link>
     </StyledGame>
   );
--- src/components/util.js
+++ src/components/util.js
@@ -1,9 +1,9 @@
 export const smallImage = (imagePath, size) => {
-    const image = imagePath.match(/media\/screenshots/)
-      ? imagePath.replace(
-          "media/screenshots",
-          `media/resize/${size}/-/screenshots`
-        )
-      : imagePath.replace("/media/games/", `/media/resize/${size}/-/games/`);
-    return image;
-  };
+  const image = imagePath.match(/media\/screenshots/)
+    ? imagePath.replace(
+        "media/screenshots",
+        `media/resize/${size}/-/screenshots`
+      )
+    : imagePath.replace("/media/games/", `/media/resize/${size}/-/games/`);
+  return image;
+};
